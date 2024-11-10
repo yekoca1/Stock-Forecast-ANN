920853471916bb523a56665c93e97b3a4d497bdd
Yunus Emre Koca <ykoca.emre@gmail.com>
2024-11-10 13:20:52 +0300

    Basic ANN is created
    Basic ANN is created. It predicts stock prices and visualize with graphics.

---
+++ HisseTahmin/# Gerekli kütüphaneleri yükleyin.py
@@ -0,0 +1,10 @@
+# Gerekli kütüphaneleri yükleyin
+!pip install yfinance
+!pip install tensorflow
+!pip install matplotlib
+!pip install numpy==1.23.5
+!pip install pyqt5==5.12.3
+!pip install pyqtwebengine==5.12.1
+!pip install daal==2021.4.0
+!pip install scipy==1.7.3
+!pip install tensorflow==2.17.0
